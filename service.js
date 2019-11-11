ae08c9860d65e29f10d4cf6d0339ece43d25941d
Pierre Charlier <chrprr1@forem.be>
2019-11-11 16:41:59 +0100

    update services.js fetch

--- service.js
+++ service.js
@@ -10,4 +10,8 @@ self.addEventListener('activate', event => {
 return self.clients.claim();
 });
 
+self.addEventListener('fetch', function(event) {
+  // C'est là que la magie opère, Noël !
+});	
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
